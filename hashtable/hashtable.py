b1818f9678ebbd0c45a8bb36db2b725dfa332aac
Evans Ibok <evansibok@gmail.com>
2020-05-28 21:53:07 +0100

    Complete put and get for day 2

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -8,6 +8,9 @@ class HashTableEntry:
         self.value = value
         self.next = None
 
+    def __repr__(self):
+        return f"key: {self.key}, value: {self.value}"
+
 
 class HashTable:
     """
@@ -50,7 +53,7 @@ class HashTable:
         hash = 5381
         for c in key:
             hash = ((hash << 5) + hash) + ord(c)
-            # hash &= 0x7F
+            hash &= 0x7FFFFFFF
             return hash
 
     def hash_index(self, key):
@@ -70,11 +73,37 @@ class HashTable:
         Implement this.
         """
         # WITHOUT COLLISION
+        # index = self.hash_index(key)
+        # if self.storage[index] != None:
+        #     print(
+        #         f"Collision! Overwriting from '{self.storage[index]}' to '{value}'")
+        # self.storage[index] = value
+
+        # WITH COLLISION
         index = self.hash_index(key)
-        if self.storage[index] != None:
-            print(
-                f"Collision! Overwriting from '{self.storage[index]}' to '{value}'")
-        self.storage[index] = value
+        node = HashTableEntry(key, value)
+
+        # Handle when the index is empty
+        # if storage index is empty:
+        if not self.storage[index]:
+            # -> Add the node
+            self.storage[index] = node
+
+        # if the index is not empty
+        else:
+            # get the current node
+            cur_node = self.storage[index]
+            # while the current node's next is not empty
+            while cur_node:
+                # Handle when the key already exists (overwrite the value)
+                if cur_node.key == key:
+                    cur_node.value = value
+                    break
+                # Handle when the key doesn't exist (add the new node)
+                if cur_node.next:
+                    cur_node = cur_node.next
+                else:
+                    cur_node.next = node
 
     def delete(self, key):
         """
@@ -85,8 +114,12 @@ class HashTable:
         Implement this.
         """
         # WITHOUT COLLISION
-        index = self.hash_index(key)
-        self.storage[index] = None
+        # if not key:
+        #     return
+        # else:
+        #     index = self.hash_index(key)
+        #     self.storage[index] = None
+        pass
 
     def get(self, key):
         """
@@ -96,12 +129,34 @@ class HashTable:
 
         Implement this.
         """
-        # WITHOUT COLLISION
-        if not key:
+        # # WITHOUT COLLISION
+        # if not key:
+        #     return None
+        # else:
+        #     index = self.hash_index(key)
+        #     return self.storage[index]
+
+        index = self.hash_index(key)
+        # Handle when index is empty
+        if not self.storage[index]:
             return None
-        else:
-            index = self.hash_index(key)
-            return self.storage[index]
+
+        # If index is not empty
+        # Let's move through the node while there's a next node
+        cur_node = self.storage[index]
+        while cur_node:
+            # cur_node = cur_node.next
+            # Handle when the key is found
+            if key == cur_node.key:
+                # -> return the value
+                return cur_node.value
+            elif cur_node.next != None:
+                # keep moving through the nodes
+                cur_node = cur_node.next
+            # if the key is not found
+            else:
+                # -> return f"{key} not found!"
+                return f"{key} not found!"
 
     def resize(self):
         """
@@ -110,6 +165,15 @@ class HashTable:
 
         Implement this.
         """
+        # number_of_entries = ?
+        # storage_size = self.capacity
+        # lf = number_of_entries / storage_size
+        # if lf > 0.7:
+        # Double the storage by 2
+        # elif lf < 0.2:
+        # Reduce the storage size by 2
+        # else:
+        # return
         pass
 
 
@@ -140,7 +204,7 @@ if __name__ == "__main__":
     # Test storing beyond capacity
     print('fox get', ht.get("firefox"))
     print('edge get', ht.get("edge"))
-    print('brave', ht.get("brave"))
+    print('brave get', ht.get("brave"))
 
     # Test resizing
     old_capacity = len(ht.storage)
---
+++ hashtable/new_ht.py
@@ -0,0 +1,197 @@
+class HashTableEntry:
+    """
+    Hash Table entry, as a linked list node.
+    """
+
+    def __init__(self, key, value):
+        self.key = key  # self.head = self.key
+        self.value = value  # value = self.value
+        self.next = None
+
+
+class HashTable:
+    """
+    A hash table that with `capacity` buckets
+    that accepts string keys
+    Implement this.
+    """
+
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.storage = [None] * capacity
+
+    """
+    The core of the FNV-1 hash algorithm is as follows:
+        hash = offset_basis
+        for each octet_of_data to be hashed
+            hash = hash * FNV_prime
+            hash = hash xor octet_of_data
+        return hash
+    -----------------------------------------------------------
+    hash: n bit unsigned integer, where n is bit length of hash
+    The multiplication is performed modulo 2^n where n is bit length of hash
+    The xor is performed on the low order octect (8 bits) of hash
+    The FNV_prime is dependent on n, the size of the hash:
+        32 bit FNV_prime = 224 + 28 + 0x93 = 16777619
+        64 bit FNV_prime = 240 + 28 + 0xb3 = 1099511628211
+    Part of the magic of FNV is the selection of the FNV_prime for a given sized unsigned integer. Some primes do hash better than other primes for a given integer size.
+    The offset_bases for FNV-1 is dependent on n, the size of the hash:
+        32 bit offset_basis = 2166136261
+        64 bit offset_basis = 14695981039346656037
+    """
+
+    def fnv1(self, key):
+        """
+        FNV-1 64-bit hash function
+        Implement this, and/or DJB2.
+        """
+
+    """
+    DJB2 Algorithm
+    hash = 0;
+    for (each character)
+        hash = (hash * 33) + (the character); 
+        // or 
+        // hash = (((hash << 5) + hash) + (unicode point of the character))
+    hash_index = hash & ((some power of two) - 1);
+    """
+
+    def djb2(self, key):
+        """
+        DJB2 32-bit hash function
+        Implement this, and/or FNV-1.
+        """
+        hash = 5381
+        for c in key:
+            # get the unicode point of the character and hash
+            hashed = ((hash << 5) + hash) + ord(c)
+            # Make sure number is positive and clamp
+            # hashed = hash & 0x7fffffff
+        return hashed
+
+    def hash_index(self, key):
+        """
+        Take an arbitrary key and return a valid integer index
+        between within the storage capacity of the hash table.
+        """
+        # return self.fnv1(key) % self.capacity
+        return self.djb2(key) % self.capacity
+
+    def put(self, key, value):
+        """
+        Store the value with the given key.
+        Hash collisions should be handled with Linked List Chaining.
+        Implement this.
+        """
+        # WITHOUT COLLISION
+        # index = self.hash_index(key)
+        # self.storage[index] = value
+
+        # WITH COLLISION
+        index = self.hash_index(key)
+        node = HashTableEntry(key, value)
+
+        # If storage index is empty
+        if not self.storage[index]:
+            # Attach a node to the index
+            self.storage[index] = node
+
+        # If index is not empty
+        else:
+            # set it as current node
+            cur_node = self.storage[index]
+        # move through the node while index isn't empty
+            while cur_node:
+                # compare the key
+                # if the current node's key is same as input key
+                if cur_node.key == key:
+                    # replace the current node's value with the new value
+                    cur_node.value = value
+                    break
+                if cur_node.next:
+                    cur_node = cur_node.next
+                else:
+                    cur_node.next = node
+
+    def delete(self, key):
+        """
+        Remove the value stored with the given key.
+        Print a warning if the key is not found.
+        Implement this.
+        """
+        index = self.hash_index(key)
+        self.storage[index] = None
+
+    def get(self, key):
+        """
+        Retrieve the value stored with the given key.
+        Returns None if the key is not found.
+        Implement this.
+        """
+        # WITHOUT COLLISION
+        # if not key:
+        #     return None
+        # index = self.hash_index(key)
+        # return self.storage[index]
+
+        # WITH COLLISION
+        # hash the key to get an index
+        index = self.hash_index(key)
+
+        # if storage index doesn't exist
+        if not self.storage[index]:
+            # return None
+            return None
+
+        # if it exists
+        cur_node = self.storage[index]
+        # while current node's next is not None
+        while True:
+            # if current node's key is same as key
+            if cur_node.key == key:
+                # return current node's value
+                return cur_node.value
+            elif cur_node.next != None:
+                cur_node = cur_node.next
+            else:
+                return f"{key} not found!"
+
+    def resize(self):
+        """
+        Doubles the capacity of the hash table and
+        rehash all key/value pairs.
+        Implement this.
+        """
+        # double capacity size
+        self.storage = self.storage * 2
+
+
+if __name__ == "__main__":
+    ht = HashTable(2)
+
+    ht.put("line_1", "Tiny hash table")
+    ht.put("line_2", "Filled beyond capacity")
+    ht.put("line_3", "Linked list saves the day!")
+
+    print('storage', ht.storage[0].next.value)
+
+    print("")
+
+    # Test storing beyond capacity
+    print(ht.get("line_1"))
+    print(ht.get("line_2"))
+    print(ht.get("line_3"))
+
+    # Test resizing
+    old_capacity = len(ht.storage)
+    ht.resize()
+    new_capacity = len(ht.storage)
+
+    print(f"\nResized from {old_capacity} to {new_capacity}.\n")
+
+    # Test if data intact after resizing
+    print(ht.get("line_1"))
+    print(ht.get("line_2"))
+    print(ht.get("line_3"))
+
+    print("")
